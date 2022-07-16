bd9c3fd8dba1fd934ed9a2f1446e8453be9eb38f
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-07-16 19:00:50 -0300

    Criação do atributo e do método Getter

--- src/com/github/lucasdevrj/fahsius/conversor/ConversorTemperatura.java
+++ src/com/github/lucasdevrj/fahsius/conversor/ConversorTemperatura.java
@@ -2,5 +2,9 @@ package com.github.lucasdevrj.fahsius.conversor;
 
 public class ConversorTemperatura {
 
+	private double temperatura;
 	
+	public double getTemperatura() {
+		return temperatura;
+	}
 }
