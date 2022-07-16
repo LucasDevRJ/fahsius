f932361207593a0c04dbcff6b565ab80bbcca56e
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-07-16 19:22:03 -0300

    Criação do método escolhaTemperatura na classe ConversorTemperatura

--- src/com/github/lucasdevrj/fahsius/conversor/ConversorTemperatura.java
+++ src/com/github/lucasdevrj/fahsius/conversor/ConversorTemperatura.java
@@ -1,13 +1,24 @@
 package com.github.lucasdevrj.fahsius.conversor;
 
+import java.util.Scanner;
+
 public class ConversorTemperatura {
 
 	private double temperatura;
 	
+	Scanner entrada = new Scanner(System.in);
+	
 	public void exibeMensagem() {
 		System.out.println("Seja bem-vindo ao conversor Fahsius");
+		escolhaTemperatura();
 	}
 	
+	private void escolhaTemperatura() {
+		System.out.println("----------|Conversões|----------");
+		System.out.println("1 - Fahrenheit para Celsius");
+		System.out.println("2 - Celsius para Fahrenheit");
+	}
+
 	public double getTemperatura() {
 		return temperatura;
 	}
