66664f5cbe5bad2dddf743b09489259174394384
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-07-17 11:55:49 -0300

    Adição do método converteFahrenheitParaCelsius na classe

--- src/com/github/lucasdevrj/fahsius/conversor/ConversorTemperatura.java
+++ src/com/github/lucasdevrj/fahsius/conversor/ConversorTemperatura.java
@@ -1,15 +1,19 @@
 package com.github.lucasdevrj.fahsius.conversor;
 
+import java.util.Scanner;
+
 public class ConversorTemperatura {
 
 	private double temperatura;
+	
+	Scanner entrada = new Scanner(System.in);
 	
 	public void exibeMensagem() {
 		System.out.println("Seja bem-vindo ao conversor Fahsius");
 		escolhaTemperatura();
 	}
 	
-	private void escolhaTemperatura() {
+	public void escolhaTemperatura() {
 		System.out.println("----------|Conversões|----------");
 		System.out.println("1 - Fahrenheit para Celsius.");
 		System.out.println("2 - Fahrenheit para Kelvin.");
@@ -17,6 +21,20 @@ public class ConversorTemperatura {
 		System.out.println("4 - Celsius para Kelvin.");
 		System.out.println("5 - Kelvin para Fahrenheit.");
 		System.out.println("6 - Kelvin para Celsius.");
+		System.out.println("-------------------------------");
+		
+		System.out.print("Digite sua opção desejada: ");
+		int opcao = entrada.nextInt();
+		
+		switch (opcao) {
+			case 1:
+				converteFahrenheitParaCelsius();
+			break;
+		}
+	}
+
+	public void converteFahrenheitParaCelsius() {
+		
 	}
 
 	public double getTemperatura() {
